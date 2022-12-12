f46992cf59fe5a3587ddd7a1c075050445228cb5
Adeola113 <Adeolabello537@gmail.com>
2022-12-12 04:11:31 -0800

    alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,5 +25,5 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	return (0);
+	return (0); 
 }
