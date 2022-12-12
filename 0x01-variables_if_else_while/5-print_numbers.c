a554b943eb3e9ff3890aeef1b2a40960246f5942
Adeola113 <Adeolabello537@gmail.com>
2022-12-12 05:26:24 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
