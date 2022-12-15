24c96a965bf8f4980d2bf2e9353f5245ad9c0be7
Adeola113 <Adeolabello537@gmail.com>
2022-12-15 12:45:50 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,19 @@
 
 /**
  * _isupper - uppercase letters
- * @c: char to check
+ * @c: the int of the parameter of my function
  *
  * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
