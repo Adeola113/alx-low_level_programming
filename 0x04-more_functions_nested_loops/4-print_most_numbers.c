b4ae2cc7dc2cfd995a467f11522aa5f083af5624
Adeola113 <Adeolabello537@gmail.com>
2022-12-15 16:34:56 -0800

    most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a function that print the numbers, from 0 to 9
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
+
