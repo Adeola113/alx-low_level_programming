83b00fe713d88fed2918456c467b9601710c94c0
Adeola113 <Adeolabello537@gmail.com>
2022-12-19 10:48:28 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+
+{
+
+	int i = 0;
+
+	for (; str[1] != '\0'; i++)
+
+	{
+
+		if ((i % 2) == 0)
+			_putchar(str[1]);
+
+		else
+
+			continue;
+
+	}
+	_putchar('\n');
+
+}
