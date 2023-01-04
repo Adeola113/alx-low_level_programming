3138f29d0cb792ee13a848816d889690c44d5ef9
Adeola113 <Adeolabello537@gmail.com>
2023-01-04 10:10:38 -0800

    task 2

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+
+}
