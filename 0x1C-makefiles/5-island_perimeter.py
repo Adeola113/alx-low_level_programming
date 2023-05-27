77e8c9e28054df71d1be7758466867c1650a4ce1
Adeola113 <adeolabello537@gmail.com>
2023-05-27 11:52:38 +0000

    task update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,13 +8,13 @@ def num_water_neighbors(grid, i, j):
     num = 0
 
     if i <= 0 or not grid[i - 1][j]:
-	num += 1
+        num += 1
     if j <= 0 or not grid[i][j - 1]:
-	num += 1
+        num += 1
     if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-	num += 1
+        num += 1
     if i >= len(grid) - 1 or not grid[i + 1][j]:
-	num += 1
+        num += 1
 
     return num
 
@@ -25,8 +25,8 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-	    if grid[i][j]:
-	       perimeter += num_water_neighbors(grid, i, j)
+            if grid[i][j]:
+                perimeter += num_water_neighbors(grid, i, j)
 
     return perimeter
 
